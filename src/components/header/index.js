209ce266eee36920259e16e65304ba2607798899
Vitor <vitorbarbosa1998@hotmail.com>
2020-05-25 10:36:03 +0100

    feat: added header component

---
+++ src/components/header/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
+
+import "./styles.css";
+
+function header() {
+  return (
+    <header>
+      <div className="container">
+        <img
+          src="https://img.icons8.com/cotton/64/000000/card-wallet.png"
+          alt="logo"
+        />
+        <div className="menu-section">
+          <nav>
+            <ul>
+              <li>
+                <Link className="back-link" to="/test">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link className="back-link" to="/test">
+                  Opçao1
+                </Link>
+              </li>
+              <li>
+                <Link className="back-link" to="/test">
+                  Opçao2
+                </Link>
+              </li>
+              <li>
+                <Link className="back-link" to="/test">
+                  Opçao3
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default header;
--- src/page/dasboard/index.js
+++ src/page/dasboard/index.js
@@ -2,11 +2,14 @@ import React from "react";
 
 import Cardtext from "../../components/card/index";
 import Pie from "../../components/pie/index";
+import Header from "../../components/header/index";
+
 import "./styles.css";
 
 function dasboard() {
   return (
     <div>
+      <Header />
       <h1>Dasboard</h1>
       <Cardtext
         icon={"https://img.icons8.com/ios-filled/50/000000/price-tag-euro.png"}
