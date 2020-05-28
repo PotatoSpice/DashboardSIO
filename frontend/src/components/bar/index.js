37a4196aa3fd884ebb1b11bd4de3a57fd68c1a7f
Vitor <vitorbarbosa1998@hotmail.com>
2020-05-28 10:14:28 +0100

    feat: added bar component

---
+++ frontend/src/components/bar/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+function bar({ labels, data, set }) {
+  let bar = `https://quickchart.io/chart?c={type:'bar',data:{labels:${labels},datasets:[{label:'${set}',data:${data}}]}}`;
+  return <img src={bar} alt="Bar" />;
+}
+
+export default bar;
