d02946c793223fe5bcb6c30c506b5fa2e0b4f9a9
JoseBaltar <josebaltar_17@hotmail.com>
2020-06-01 22:29:27 +0100

    updated: SAF-T upload and save
    + Added multer dependency

--- backend/api/controllers/parsingController.js
+++ backend/api/controllers/parsingController.js
@@ -1,6 +1,53 @@
+const fs = require('fs').promises;
+
+const Saft = require('../models/Saft');
+
+const parseAndValidateSaft = async function(fileDetails, fileData) {
+    var jsonSaft = {};
+    // # simple parse
+    // https://github.com/Leonidas-from-XIV/node-xml2js
+    // # parse & validation
+    // https://github.com/libxmljs/libxmljs
+    // https://stackoverflow.com/questions/14856643/validating-xml-against-a-schema-xsd-in-nodejs
+
+    var dummy = false;
+    if (dummy) { // check SAF-T file version
+        throw new Error('Versão do SAF-T desatualizada. Deverá ser enviado um documento com a última versão disponível.')
+    }
+    else if (dummy) { // check SAF-T with XSD
+        throw new Error('SAF-T não está de acordo com as normas! Ver definição do XSD Schema para a versão do documento.')
+    } else {
+        return jsonSaft;
+    }
+}
 
 const uploadParseSaft = async (req, res, next) => {
+    if (!req.file) {
+        next({
+            message: 'Bad Request. SAF-T não foi recebido!',
+            status: 400
+        })
+    } else {
+        // # Ler o ficheiro SAF-T recebido pelo 'multer'
+        const tempXmlSaft = await fs.readFile(req.file.path).catch(next);
 
+        // # Validar SAF-T XML com XSD e Parse para JSON
+        const jsonSaft = await parseAndValidateSaft(req.file, tempXmlSaft).catch(next);
+
+        // # Guardar o SAF-T temporario
+        const xmlFile = `./saft/${ Date.now() }_SAFT_${ jsonSaft.Header.AuditFileVersion }_${ jsonSaft.Header.FiscalYear }.xml`,
+                jsonFile = `./saft/${ Date.now() }_SAFT_${ jsonSaft.Header.AuditFileVersion }_${ jsonSaft.Header.FiscalYear }.json`;
+        
+        await Promise.all([
+            fs.writeFile(xmlFile, tempXmlSaft), 
+            fs.writeFile(jsonFile, jsonSaft)]).catch(next);
+        // await fs.unlink(req.file.path); // remover o ficheiro temporario (opcional)
+
+        // # Como não se vai modelar a BD, colocar o JSON na collection 'Saft'
+        await Saft(jsonSaft).save().catch(next);
+
+        res.download(jsonFile, 'Parsed_SAF-T.json');
+    }
 }
 
 module.exports = {
---
+++ backend/config/multer.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const multer = require('multer')
+
+// Filtro para os ficheiros de resultado do diagnóstico (only .pdf)
+const saftFilter = (_, file, cb) => {
+    const fileExt = path.extname(file.originalname);
+    const allowedMimes = [
+        'application/xml',
+        'text/xml'
+    ];
+
+    if(fileExt === '.xml' && allowedMimes.includes(file.mimetype)) {
+        cb(null, true);
+    } else {
+        cb(new Error('Invalid file type. Only XML (SAF-T) files are allowed!'));
+    }
+}
+
+// Configuração MULTER STORAGE
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, './saft/temp')
+	},
+	filename: function (req, file, cb) {
+		req.uploadedFile = `${ file.originalname }`
+		cb(null, `${ file.fieldname }-${ Date.now() }-temp`)
+    }
+})
+
+// Configuração MULTER
+const upload = multer({ 
+    storage: storage,
+    fileFilter: saftFilter
+})
+
+module.exports = upload
