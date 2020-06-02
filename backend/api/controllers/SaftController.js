78e1b4b757c37d1e51386190874c120a853e8b48
SterbenCyrodiil <rodrigo.coelho1999@gmail.com>
2020-06-02 01:04:48 +0100

    Update: Corrigido problema com os find() que deveriam ser update()

--- backend/api/controllers/SaftController.js
+++ backend/api/controllers/SaftController.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const saft = require('../models/Saft');
 
 const getCompanyInfo = async (req, res) => {
-    const companyInfo = await saft.find({ 'AuditFile.Header.FiscalYear': req.body.FiscalYear }, 
-    {'$project': {'info': AuditFile.Header }})
+    const companyInfo = await saft.find({ 'AuditFile.Header.FiscalYear': req.body.FiscalYear },
+        { '$project': { 'info': AuditFile.Header } })
 }
 
 const getJSON = async (req, res) => {
@@ -27,29 +27,24 @@ const getJSON = async (req, res) => {
 //Retorna informação referente a cada Movimentação: Ou seja, valores de  compras() e vendas(organizadas por período)
 const getInfo = async (req, res) => {
 
-    const jsonInvoicesMonth = await saft.find({ 'AuditFile.Header.FiscalYear': req.body.FiscalYear }, {
-        '$expr': {
-            '$group': {
+    const jsonInvoicesMonth = await saft.aggregate({ $match: { 'AuditFile.Header.FiscalYear': req.body.FiscalYear } },
+        {
+            $group: {
                 'Period': '$AuditFile.SourceDocuments.SalesInvoices.Invoice.Period',
                 'MonthTotal': { '$sum': '$AuditFile.SourceDocuments.SalesInvoices.Invoice.DocumentTotals.GrossTotal' },
                 'MonthNetTotal': { '$sum': '$AuditFile.SourceDocuments.SalesInvoices.Invoice.DocumentTotals.NetTotal' }
-            },
-            '$sort': {
-                'Period': 1
             }
+        },
+        { $sort: { 'Period': 1 } }
+    )
 
-        }
-    });
-
-    const jsonTotalPurchases = await saft.find({ 'AuditFile.Header.FiscalYear': req.body.FiscalYear },
-        { 'AuditFile.GeneralLedgerEntries': { '$slice': [3, 1] } }, {
-        '$expr': {
-            '$group': {
+    const jsonTotalPurchases = await saft.aggregate({ $match: { 'AuditFile.Header.FiscalYear': req.body.FiscalYear } },
+        {
+            $group: {
                 'Journal': 'AuditFile.GeneralLedgerEntries.Journal',
                 'PurchaseTotal': { '$sum': '$AuditFile.GeneralLedgerEntries.Journal.Transaction.Lines.CreditLine.CreditAmount' },
-            },
-        }
-    });
+            }
+        })
 
     res.json(
         jsonTotalPurchases,
@@ -58,41 +53,40 @@ const getInfo = async (req, res) => {
 
 //Search for a Client/Customer using its ID
 const getClientInfo = async (req, res) => {
-    const clientInfo = await saft.find({ 'AuditFile.Header.FiscalYear': req.body.FiscalYear, 'AuditFile.MasterFiles.Customer.CustomerID': req.params.id })
-    res.json( clientInfo)
+    const clientInfo = await saft.aggregate({ 'AuditFile.Header.FiscalYear': req.body.FiscalYear, 'AuditFile.MasterFiles.Customer.CustomerID': req.params.id })
+    res.json(clientInfo)
 }
 
 //Search for a Supplier using its ID
 const getSupplierInfo = async (req, res) => {
     const requestInfo = await saft.find({ 'AuditFile.Header.FiscalYear': req.body.FiscalYear, 'AuditFile.MasterFiles.Supplier.SupplierID': req.params.id })
-    res.json( requestInfo )
+    res.json(requestInfo)
 }
 
 //Search for a specific invoice using its ID (shares all information)
 const getInvoice = async (req, res) => {
     const invoiceInfo = await saft.find({ 'AuditFile.Header.FiscalYear': req.body.FiscalYear, 'AuditFile.SourceDocuments.SalesInvoices.Invoice.InvoiceNo': req.params.id })
-    res.json( invoiceInfo )
+    res.json(invoiceInfo)
 }
 
 //Get all Invoices
 const getInvoices = async (req, res) => {
-    const invoices = await saft.find({ 'AuditFile.Header.FiscalYear': req.body.FiscalYear },
+    const invoices = await saft.aggregate({ 'AuditFile.Header.FiscalYear': req.body.FiscalYear },
         { 'AuditFile.SourceDocuments.SalesInvoices': { '$slice': [3, -1] } })
-    res.json( invoices )
+    res.json(invoices)
 }
 
 //Get expenditure by costumer
 const getSales = async (req, res) => {
-    const salesPerClient = await saft.find({ 'AuditFile.Header.FiscalYear': req.body.FiscalYear },
+    const salesPerClient = await saft.aggregate({ $match: { 'AuditFile.Header.FiscalYear': req.body.FiscalYear } },
         {
-            '$expr': {
-                '$group': {
-                    'Customer': '$AuditFile.SourceDocuments.SalesInvoices.Invoice.CustomerID',
-                    'ClientTotal': { '$sum': '$AuditFile.SourceDocuments.SalesInvoices.Invoice.DocumentTotals.GrossTotal' },
-                    'ClientNetTotal': { '$sum': '$AuditFile.SourceDocuments.SalesInvoices.Invoice.DocumentTotals.NetTotal' }
-                }
+            $group: {
+                'Customer': '$AuditFile.SourceDocuments.SalesInvoices.Invoice.CustomerID',
+                'ClientTotal': { '$sum': '$AuditFile.SourceDocuments.SalesInvoices.Invoice.DocumentTotals.GrossTotal' },
+                'ClientNetTotal': { '$sum': '$AuditFile.SourceDocuments.SalesInvoices.Invoice.DocumentTotals.NetTotal' }
             }
         })
+
     res.json(
         salesPerClient
     );
@@ -101,10 +95,8 @@ const getSales = async (req, res) => {
 
 //Search for a product using its ID
 const getProductInfo = async (req, res) => {
-    const productInfo = await saft.find({ 'AuditFile.Header.FiscalYear': req.body.FiscalYear, 'AuditFile.MasterFiles.Product.ProductCode': req.body.id },
-        {
-            '$project': {'Products': 'AuditFile.MasterFiles.Product'}
-        })
+    const productInfo = await saft.find({ 'AuditFile.Header.FiscalYear': req.body.FiscalYear, 'AuditFile.MasterFiles.Product.ProductCode': req.body.id })
+    
     res.json(
         productInfo
     );
